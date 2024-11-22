b2c261e1843f31ec3bb43b8a9b26da05f47fe5c7
Shubham Raj <shubhamraj328@gmail.com>
2024-11-22 23:10:08 +0530

    connected to my DB

--- common.py
+++ common.py
@@ -7,8 +7,8 @@ def connect_db():
         return mysql.connector.connect(
             host="localhost", 
             user="root", 
-            password="ehnd11", 
-            database="venue_booking_system"
+            password="password", 
+            database="venue_booking"
         )
     except mysql.connector.Error as err:
         messagebox.showerror("Database Connection Error", f"Unable to connect to the database: {err}")
